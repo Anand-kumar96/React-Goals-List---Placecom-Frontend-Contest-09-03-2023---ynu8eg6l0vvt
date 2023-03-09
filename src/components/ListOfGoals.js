1dc75ee9fc749f29a0b259149b80dd20872c5af9
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-09 20:01:37 +0530

    Update ListOfGoals.js

--- src/components/ListOfGoals.js
+++ src/components/ListOfGoals.js
@@ -3,7 +3,8 @@ import React from "react";
 const ListOfGoals = ({goals}) => {
     return (
       <ul>
-        {goals.map((goal,index)=>(<li key={index+1}><p>My goal is to {goal.goal}, by {goal.by}.</p></li>))}
+        {goals.map((goal,idx) => (<li key={idx+1}><p>My goal is to {goal.goal}, by {goal.by}.</p></li>))}
+        
       </ul>
     );
 }
