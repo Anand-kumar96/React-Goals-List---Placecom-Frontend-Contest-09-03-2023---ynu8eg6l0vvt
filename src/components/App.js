1b7a48e200a737835a22a35f2b9518014bac6c10
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-09 19:42:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,3 +18,30 @@ const App = () => {
 
 
 export default App;
+const [allGoals, setAllGoals] = useState([]);
+
+  const goalHandler = (goals)=>{
+
+    return setAllGoals((prevGoals)=>[...prevGoals, goals]);
+
+  }
+
+  return (
+
+    <div id="main">
+
+      <GoalForm  onGoalHandler={goalHandler}/>
+
+      {/* {allGoals.map((goal,idx) => <ListOfGoals key={goal.idx} mygoal={goal.goal} bygoal={goal.by}/>)} */}
+
+      <ListOfGoals goals={allGoals}/>
+
+    </div>
+
+  )
+
+  
+
+}
+
+export default App;
